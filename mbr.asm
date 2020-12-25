ecca087d786292660061c82eba01561dc85304dd
tzg157 <tzg157@gmail.com>
2020-12-25 17:54:52 +0800

    添加保护模式下，清屏及打印字符功能

--- mbr.asm
+++ mbr.asm
@@ -17,14 +17,18 @@ _start:
 	movb $0x0, %bh
 	movw $0x0, %dx
 	call set_currsor
-	call clean_screen
+	call clean_screen_new
 	
 	call set_bgcolor
 
     call print
 	
-loop:
-    jmp loop
+	movb	$0x86,	%ah   #延时
+	movw	$0x001e,	%cx
+	movw	$0x8480,	%dx
+	int	$0x15
+	
+	jmp enable_a20
 
 set_bgcolor:
 	movw $0x07d0, %cx	# 打印次数
@@ -69,8 +73,125 @@ clean_screen:
     int     $0x10  
     ret 
 	
+clean_screen_new:               
+    movb    $0x3,		%al     # 
+    movb    $0x0,		%ah     #  
+    int     $0x10  
+    ret 
+
+### 通过键盘控制器的0x64及0x60端口使能 实模式到保护模式的转换
+### 还能通过南桥0x92端口判断
+enable_a20:
+
+	inb	$0x92, %al		## 进入南桥芯片
+	orb	$0x2, %al			## 打开南桥芯片的第2位(A20控制位)
+	outb %al, $0x92 		## 将改动过的配置送回南桥芯片
+	jmp preswitch
+	
+	### 0x64端口
+enable_a20.1:	
+	inb	$0x64,	%al
+	testb	$0x2,	%al
+	jnz	enable_a20.1
+	movb	$0xd1,	%al			# write command
+	outb	%al,	$0x64
+	
+enable_a20.2:
+	inb	$0x64,	%al
+	testb	$0x2,	%al
+	jnz	enable_a20.2
+	movb	$0xdf,	%al			# write command
+	outb	%al,	$0x60
+	#jmp preswitch
+	
+### load gdt
+preswitch:
+	cli
+	lgdt    gdt_ptr			# 加载gdt
+	movl    %cr0, %eax
+	orl     $0x1, %eax
+	movl    %eax, %cr0
+
+	ljmp $0x8, $protect_mode	# ljmp将清空cs中的值，避免实模式切换至保护模式时，发生一些错误
+							# 代码段段选子为 00001 000,偏移量为保护模式代码段地址
+							# 此后将工作至保护模式下
+							
+							
+### 32位保护模式 基于段选子寻址
+### TODO 基于页寻址
+.code32
+protect_mode:
+	movw    $0x10, %ax    # 数据段索引在gdt中索引下标为2，Our data segment selector
+	movw    %ax, %ds                # -> DS: Data Segment
+	movw    %ax, %es                # -> ES: Extra Segment
+	movw    %ax, %ss                # -> SS: Stack Segment
+	movw    $0, %ax                 # Zero segments not ready for use
+	movw    %ax, %fs                # -> FS
+	movw    %ax, %gs                # -> GS
+	
+	movl	$p_msg,	%edi
+	call p_printchars
+
+spin:
+	jmp	spin
+
+### 保护模式下打印字符串
+p_printchars:
+	call	p_clean
+	xorl	%ecx,	%ecx
+p_print:
+	movb	(%edi,%ecx),	%dl
+	test	%dl,	%dl
+	jz	p_print_end
+	movb	$0x9f,	%dh
+	movw	%dx,	(%ebx,%ecx,2)
+	incl	%ecx
+	jmp	p_print
+p_print_end:
+	ret
+
+### 保护模式清屏
+p_clean:
+	xorl	%ecx,	%ecx
+	movl	$0xb8000,	%ebx
+p_clearchar:
+	movb	$0x20,	%dl
+	movb	$0x9f,	%dh	
+	movl	$0x0f200f20,	(%ebx,%ecx,4)
+	incl	%ecx
+	cmpl	$1000,	%ecx
+	jl	p_clearchar
+	ret
+	
 msg:
     .ascii "Hello, TLOS!"
-    .org 510
-    .word 0xAA55
+	
+p_msg:
+	.ascii "entry protect mode!"
+
+gdt:
+	.long 0, 0
+	
+	#code segament
+	.word 0xffff	#(0-15)limit
+	.word 0x0		#(16-31)base
+	.byte 0x0		#(32-39)base
+	.byte 0x9a		#(40-47)access byte
+	.byte 0xcf		#低位(48-51)limit，高位(52-55)flags
+	.byte 0x0		#(56-63)base
+	
+	#data segament
+	.word 0xffff	#(0-15)limit
+	.word 0x0		#(16-31)base
+	.byte 0x0		#(32-39)base
+	.byte 0x92		#(40-47)access byte
+	.byte 0xcf		#低位(48-51)limit，高位(52-55)flags
+	.byte 0x0		#(56-63)base
+
+gdt_ptr:
+	.word . - gdt - 1
+	.long gdt
+	
+	.fill 0x1fe - (. - _start),1,0
+	.word 0xaa55	
 	
